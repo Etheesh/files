db476aa18f27f36e670cc4468e5b2e0e75b63f9c
Ubuntu <ubuntu@ip-172-31-86-128.ec2.internal>
2022-06-30 13:53:21 +0000

    switching from 5 to 6

--- pattern.sh
+++ pattern.sh
@@ -1,6 +1,6 @@
 o "enter file name"
 read file
-echo "please enter the pattern1.2"
+echo "please enter the pattern1.3"
 read pattern
 echo "the file name is $file"
 a=`grep -i -l "$pattern" $file | wc -w `
